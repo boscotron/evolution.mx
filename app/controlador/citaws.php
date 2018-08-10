ab2220a37b39ee61560f0b5d838616a053ad9ac4
boscotron <boscotron@users.noreply.github.com>
2018-08-10 13:01:15 -0500

    ++ch

--- app/controlador/citaws.php
+++ app/controlador/citaws.php
@@ -14,14 +14,14 @@ $out['personal'][]=['nombre'=>'Esmeralda',
                     'horario_mat_fin'=>'14',
                     'horario_ves_ini'=>'15',
                     'horario_ves_fin'=>'20',
-                    'dias_laborales'=>['lunes','martes','miercoles','jueves','viernes','sabado'],
                     'servicios'=>['cortes','tintes']
 ];
 
 $out['servicios']=['cortes','tintes','Mechas'];
 
+//$respuesta=
+//$_POST['fecha']
 
-
-echo json_encode(['POST'=>$_POST,'GET'=>$_GET,'out'=>$out]);
+echo json_encode(['POST'=>$_POST,'GET'=>$_GET,'out'=>$out,'respuera'=>$respuesta]);
 
 ?>
