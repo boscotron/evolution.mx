67242f158e84b84441358d8cbd53796e6470eb0c
boscotron <boscotron@users.noreply.github.com>
2018-08-21 14:26:07 -0500

    +ch Juan

--- templet/login.php
+++ templet/login.php
@@ -1,1 +1,11 @@
-hdhdhdh
+<div class="row" style="margin:50px 0">
+    <div class="col-md-3">
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-content">
+                <a href="https://www.facebook.com/v3.0/dialog/oauth?client_id=353311451716569&redirect_uri=https://www.comsis.mx/api/fb/re.php&state={re=<?php $this->url_inicio(); ?>login/verificar&d=<?php echo JMY_API; ?>}" class="btn btn-info"><i class="fa fa-facebook"></i> Entra con Facebook</a>
+            </div>
+        </div>
+    </div>
+</div>
