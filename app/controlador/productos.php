1697cf618bd0305e0a03a5fbe8be1d2f2d167155
boscotron <boscotron@users.noreply.github.com>
2018-08-23 13:56:56 -0500

    +CH Celina

--- app/controlador/productos.php
+++ app/controlador/productos.php
@@ -4,6 +4,7 @@ switch ($_GET['peticion']) {
 
     default:
         $jmyWeb->cargar(["pagina"=>"productos"]);
+        $jmyWeb ->cargar_js(["url"=>"templet/js/productos.js?d=".date('U')]);
         $jmyWeb ->cargar_vista(["url"=>"productos.php"]);
     break;
 }
---
+++ app/controlador/wsproductos.php
@@ -0,0 +1,52 @@
+<?php
+
+    $config=[
+        "tabla"=>'PRODUCTOS'
+    ];
+    switch ($_GET['peticion']) {
+        case 'instalar':
+            if($jmyWeb->modoEditor()){
+                $jmyWeb->pre(['p'=>$jmy->db([$config['tabla']]),'t'=>'Instalación DB']);
+            }else{
+                $out['error']='Sessión no iniciada';
+            }        
+            break;
+        case 'marcas-lista':
+            $out['ver'] = $jmy->ver([	
+                        "TABLA"=>$config['tabla'],
+                        "SALIDA"=>'ARRAY',
+                    ]);
+        break;
+        case 'marcas':
+            if($jmyWeb->modoEditor()){
+                if($_POST['nombre']!=''){
+                    $out['id']=($_POST['id']!='')?$_POST['id']:uniqid();
+                    $out['g']= $jmy->guardar([	"TABLA"=>$config['tabla'], 
+                            "ID_F"=>$out['id'], 
+                            "A_D"=>TRUE, 
+                            "GUARDAR"=>['nombre_marca'=>$_POST['nombre']],
+                        ]);
+                }
+                if($out['id']){
+                    $out['ver'] = $jmy->ver([	
+                        "TABLA"=>$config['tabla'],
+                        "ID_F"=>$out['id'], 
+                    ]);
+                }
+            }else{
+                $out['error']='Sessión no iniciada';
+            }
+            break;        
+        default:
+            # code...
+            break;
+    }
+
+
+
+echo json_encode([  
+    'POST'=>$_POST,
+    'GET'=>$_GET,
+    'out'=>$out
+    ]);
+    ?>
