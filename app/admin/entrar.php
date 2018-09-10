9870902244f52fec8abc0181afa42d88f275f1aa
boscotron <boscotron@users.noreply.github.com>
2018-09-10 17:40:42 -0500

    +ch bsk
    Inicio de update login V1 oAuth

--- app/admin/entrar.php
+++ app/admin/entrar.php
@@ -1,8 +1,13 @@
 <?php
 require(BASE_APP.'class/jmy3webAdmin.class.php');
 $out = new JmyWebSession();
-$tkn = $out->session();
-$jmyWeb ->pre(['p'=>$tkn,'t'=>'tkn']);
+//$tkn = $out->session();
+$jmyWeb ->pre(['p'=>$_GET,'t'=>'get']);
+$enlace_actual = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['PATH_INFO'];
+$jmyWeb ->pre(['p'=>$enlace_actual ,'t'=>'get']);
+
+
+/*
 if($tkn['o']['out']['error']!=''){
 $jmyWeb ->pre(['p'=>$tkn,'t'=>'Error:']);
 }else{
@@ -10,7 +15,7 @@ $fn = (!$_SESSION['JMY3WEB'][DOY])? $out->fn(["fn"=>"codigo","token"=>$tkn['toke
 if($fn['out']['error']!='')
 	$jmyWeb ->pre(['p'=>$fn,'t'=>'Error:']);
 }
-if($_GET['peticion']=='salir'){$_SESSION['JMY3WEB'][DOY]=0;}
+if($_GET['peticion']=='salir'){$_SESSION['JMY3WEB'][DOY]=0;}*/
 ?>
 <html>
 <head>
@@ -19,7 +24,7 @@ if($_GET['peticion']=='salir'){$_SESSION['JMY3WEB'][DOY]=0;}
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity=
 	"sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-	<script src="<?php $jmyWeb->url_inicio(); ?>app/js/jmy/jmyWebCon.js" ></script>	
+	<!--<script src="<?php $jmyWeb->url_inicio(); ?>app/js/jmy/jmyWebCon.js" ></script>	-->
 	<style>
 		body{
 			background-color: #3c5478;
@@ -44,12 +49,8 @@ if($_GET['peticion']=='salir'){$_SESSION['JMY3WEB'][DOY]=0;}
 				</div>
 				<div class="card-body">
 					<p>
-						<a class="btn btn-success"  href="https://comsis.mx/app/comsisAdmin/" target="_blank" >Abrir panel</a>
-						<button class="btn btn-info hidden"  id="refrescar" >Refrescar</button>
-					</p>
-					<p>
-					 <?php echo ($fn['out']['error']!='')?$fn['out']['error'] : 'Registra este código en tu panel para poder editar esta web'; ?>
-					<h2 style="color:indigo"> <?php echo $fn['out']['codigo']['codigo']; ?></h2>
+						<a class="btn btn-success"  href="https://comsis.mx/app/entrar/?re=<?php $jmyWeb->url_inicio(); ?>/entrar/&api=e2ad454bea7d919f0fc411a8b885580c&api_web=<?php echo JMY_API; ?>&sep=1" target="_blank" >Entrar</a>
+						
 					</p>
 				</div>
 			</div>
