7126cff13038a80fe99164da3524f3ab46791736
boscotron <boscotron@users.noreply.github.com>
2018-09-10 19:17:17 -0500

    + CH Login

--- app/admin/entrar.php
+++ app/admin/entrar.php
@@ -1,60 +1,63 @@
 <?php
 require(BASE_APP.'class/jmy3webAdmin.class.php');
 $out = new JmyWebSession();
-//$tkn = $out->session();
-$jmyWeb ->pre(['p'=>$_GET,'t'=>'get']);
-$enlace_actual = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['PATH_INFO'];
-$jmyWeb ->pre(['p'=>$enlace_actual ,'t'=>'get']);
-
-
-/*
-if($tkn['o']['out']['error']!=''){
-$jmyWeb ->pre(['p'=>$tkn,'t'=>'Error:']);
-}else{
-$fn = (!$_SESSION['JMY3WEB'][DOY])? $out->fn(["fn"=>"codigo","token"=>$tkn['token']]):false;
-if($fn['out']['error']!='')
-	$jmyWeb ->pre(['p'=>$fn,'t'=>'Error:']);
-}
-if($_GET['peticion']=='salir'){$_SESSION['JMY3WEB'][DOY]=0;}*/
-?>
-<html>
-<head>
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity=
-	"sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-	<!--<script src="<?php $jmyWeb->url_inicio(); ?>app/js/jmy/jmyWebCon.js" ></script>	-->
-	<style>
-		body{
-			background-color: #3c5478;
-			padding-top:30px;
-		}
-		.card{
-			background-color:#E2F1FF;
-			padding:5px; 
-		}
-	</style>
-</head>
-<body>
-<input type="hidden" id="tiket" value="<?php echo $fn['out']['codigo']['guardar']; ?>">
-<input type="hidden" id="web" value="<?php $jmyWeb->url_inicio();?>">
-	<div class="row">
-		<div class="col-sm-12 col-md-3">
-		</div>
-		<div class="col-sm-12 col-md-6">
-			<div class="card">
-				<div class="card-head">
-					Acceso a edición web
-				</div>
-				<div class="card-body">
-					<p>
-						<a class="btn btn-success"  href="https://comsis.mx/app/entrar/?re=<?php $jmyWeb->url_inicio(); ?>/entrar/&api=e2ad454bea7d919f0fc411a8b885580c&api_web=<?php echo JMY_API; ?>&sep=1" target="_blank" >Entrar</a>
-						
-					</p>
+if($_GET['peticion']=='salir'){$out->session_activa([],1);}
+$token = explode("/",$_GET['peticion']);
+$token = $out->session_activa($token);
+if($token['permiso']!='' && $_SESSION['re_login']!=''){?>
+	<script type="text/javascript">
+	<!--
+	window.location = <?php echo $_SESSION['re_login']; ?>
+	//-->
+	</script>
+<?php unset($_SESSION['re_login']); }else{
+	//$jmyWeb ->pre(['p'=>$token,'t'=>'TOKEN']);
+	?>
+	<html>
+	<head>
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity=
+		"sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+		<!--<script src="<?php $jmyWeb->url_inicio(); ?>app/js/jmy/jmyWebCon.js" ></script>	-->
+		<style>
+			body{
+				background-color: #3c5478;
+				padding-top:30px;
+			}
+			.card{
+				background-color:#E2F1FF;
+				padding:5px; 
+			}
+		</style>
+	</head>
+	<body>
+	<input type="hidden" id="tiket" value="<?php echo $fn['out']['codigo']['guardar']; ?>">
+	<input type="hidden" id="web" value="<?php $jmyWeb->url_inicio();?>">
+		<div class="row">
+			<div class="col-sm-12 col-md-3">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="card">
+					<div class="card-head">
+						Acceso a edición web
+					</div>
+					<div class="card-body">
+						<p>
+							<?php 
+							if($token['permiso']==''){?>
+							<a class="btn btn-success"  href="https://comsis.mx/app/entrar/?re=<?php $jmyWeb->url_inicio(); ?>/entrar&api=e2ad454bea7d919f0fc411a8b885580c&api_web=<?php echo JMY_API; ?>&sep=1" >Entrar</a>
+							<?php }else{ ?>
+								<a class="btn btn-info"  href="<?php $jmyWeb->url_inicio(); ?>">Editar página</a>
+								<a class="btn btn-danger"  href="<?php $jmyWeb->url_inicio(); ?>/entrar/salir">Salir</a>
+							<?php } ?>
+							
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</body>
-</html>
+	</body>
+	</html>
+<?php } ?>
--- app/class/jmy3webAdmin.class.php
+++ app/class/jmy3webAdmin.class.php
@@ -14,6 +14,31 @@ class JmyWebSession {
 		$_SESSION['jmyWebApp']=$sj[$sj['t']];
 		return ($o['out']['error']!='')?["o"=>$o,"o2"=>$o2]:$sj[$sj['t']];
 	}
+	public function s2($d){
+		$postdata = json_encode($d);
+		$opts = ['http' =>[
+				'method'  => 'POST',
+				'header'  => 'Content-type: application/json',
+				'content' => $postdata
+			]];
+		$context  = stream_context_create($opts);
+		$result = file_get_contents($d['url'], false, $context);
+		return $result;
+	}
+	public function session_activa($d=[],$logout=true){
+		if($logout) unset($_SESSION);
+		if($d[0]!=''&&$d[1]!=''){
+		$d['id']=$d[0];$d['token']=$d[1];unset($d[0]);unset($d[1]);
+		$d['api']="e2ad454bea7d919f0fc411a8b885580c";
+		$d['api_web']=JMY_API;
+		$d['datos_device']=true;
+		$d['apis'][$d['api']]=["nombre"=>"JmyWeb","version"=>"1.0"];
+		$d['url']='https://comsis.mx/api/auth/v1/token';
+		$o=(is_array($_SESSION['jmysa']))?$_SESSION['jmysa']:json_decode($this->s2($d),1);		
+		$_SESSION['jmysa']=(is_array($_SESSION['jmysa']))?$o:["user"=>$o['out']['userData'],"devices"=>$o['out']['devices'],"body"=>$o['out']['jmyapi']['body'],"permiso"=>$o['out']['jmyapi']['body']['permisos_api']['PERMISOS']];
+		}$_SESSION['JMY3WEB'][DOY]=($_SESSION['jmysa']['permiso']>2)?1:0;
+		return $_SESSION['jmysa'];
+	}
 	public function fn($d){ //fn(["fn"=>"codigo","token"=>"codigo",])
 		$d['url']=RUTA_ACTUAL;
 		$o = ($d['fn']!='' && $d['token']!='' )?$this->s($d):["out"=>["error"=>"sin asignar token ó fn (función)","d"=>$d]];
