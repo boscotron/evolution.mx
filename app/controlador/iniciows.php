b59c1e03276e59a98c2e56c34970e9bfb13e5c19
CeliinRMtz <34458750+CeliinRMtz@users.noreply.github.com>
2018-11-20 11:14:05 -0600

    +ch celina carrusel

--- app/controlador/iniciows.php
+++ app/controlador/iniciows.php
@@ -14,3 +14,34 @@ switch ($peticion[0]) {
 }
 
 echo json_encode($salida);
+
+$peticion1 = explode('/',$_GET['peticion1']);
+
+switch ($peticion1[0]) {
+    case 'servicios1':
+        $salida1=$jmy->catalogos(["id"=>"lista_de_servicios"]);
+        $salida1= $salida1['otFm'];
+    break;
+    
+    default:
+        # code...
+        break;
+}
+
+echo json_encode($salida1);
+
+
+$peticion2 = explode('/',$_GET['peticion2']);
+
+switch ($peticion2[0]) {
+    case 'servicios2':
+        $salida2=$jmy->catalogos(["id"=>"lista_de_testimonios"]);
+        $salida2= $salida2['otFm'];
+    break;
+    
+    default:
+        # code...
+        break;
+}
+
+echo json_encode($salida2);
--- templet/js/inicio.js
+++ templet/js/inicio.js
@@ -48,7 +48,7 @@ jQuery(function($){
                         count++;
                         console.log(resultado);
                         if(resultado!=undefined)
-                            html = html + '<li><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="nombre__'+i+o+'" data-editor="no">'+resultado.nombre+'</div><span><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="precio__'+i+o+'" data-editor="no">'+((resultado.precio!=undefined)?resultado.precio:'$75')+'</div></span></li>';
+                            html = html + '<li><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="nombre__'+i+o+'" data-editor="no">'+resultado.nombre+'</div><span><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="precio__'+i+o+'" data-editor="no">'+((resultado.precio!=undefined)?resultado.precio:'£ 200')+'</div></span></li>';
 
                     }
                     html = html + '</ul></div>';
@@ -74,4 +74,135 @@ jQuery(function($){
     $(document).ready(function(){
         serviciosws();
     });
+});
+
+console.log('hola');
+
+jQuery(function($){
+
+    function cargarCarousel(carousel){
+        $('.'+carousel).carouFredSel({
+            responsive: true,
+            auto: false,
+            width: '100%',
+            height: 'variable',
+            prev: '.pricing.prev-arrow',
+            next: '.pricing.next-arrow',
+            scroll: 1,				
+            items: {
+            width: $(this).find('.column').width(),
+            height: 'variable',
+            visible: {
+                min: 1,
+                max: 2
+            }
+            }				
+        });
+    }
+
+    
+    
+    function serviciosws1(){
+        let carousel = 'gallery-details';
+        $.ajax({
+            url: location.origin + '/iniciows/servicios1/',
+            type: 'post',	
+            dataType: 'json',
+            success: function(res) {
+                console.log(res);
+                let salto_columnas=10;
+                $('.'+carousel).html('');
+                let columnas=Math.round(2+(salto_columnas/res.length));
+                let html = '';
+                console.log(columnas);
+                let count = 0;
+                let filas=Math.round(res.length/columnas);
+                console.log(filas);
+                
+                for (let i = 0; i < columnas; i++) {
+                    html = html + '<div class="dt-sc-offer"><figure class="gallery-thumb">';
+                    for (let o = 0; o < filas; o++) {
+                        const resultado = res[count];
+                        count++;
+                        console.log(resultado);
+                        if(resultado!=undefined)
+                            html = html + '<li><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="nombre__'+i+o+'" data-editor="no">'+resultado.nombre+'</div><span><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="precio__'+i+o+'" data-editor="no">'+((resultado.precio!=undefined)?resultado.precio:'$75')+'</div></span></li>';
+
+                    }
+                    html = html + '</figure></div>';
+                }
+                $('.caroufredsel_wrapper').css('height',(filas*51));
+                $('.'+carousel).html(html);
+                jmy_web_div_click();
+                console.log(html);
+                
+                cargarCarousel(carousel);
+
+            },
+            error: function(res) {
+                console.log(res);
+            },
+            data: {}
+        });
+    }
+
+
+   
+    
+    $(document).ready(function(){
+        serviciosws1();
+    });
+});
+
+
+function serviciosws2(){
+    let carousel = 'author';
+    $.ajax({
+        url: location.origin + '/iniciows/servicios2/',
+        type: 'post',	
+        dataType: 'json',
+        success: function(res) {
+            console.log(res);
+            let salto_columnas=10;
+            $('.'+carousel).html('');
+            let columnas=Math.round(2+(salto_columnas/res.length));
+            let html = '';
+            console.log(columnas);
+            let count = 0;
+            let filas=Math.round(res.length/columnas);
+            console.log(filas);
+            
+            for (let i = 0; i < columnas; i++) {
+                html = html + '<div class="dt-sc-testimonial-carousel-wrapper"><ul class="dt-sc-testimonial-carousel">';
+                for (let o = 0; o < filas; o++) {
+                    const resultado = res[count];
+                    count++;
+                    console.log(resultado);
+                    if(resultado!=undefined)
+                        html = html + '<li><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="nombre__'+i+o+'" data-editor="no">'+resultado.nombre+'</div><span><div class="jmy_web_div" data-page="'+resultado.ID_F+'" data-tabla="catalogos" id="testimonio__'+i+o+'" data-editor="no"></div></span></li>';
+
+                }
+                html = html + '</ul></div>';
+            }
+            $('.caroufredsel_wrapper').css('height',(filas*51));
+            $('.'+carousel).html(html);
+            jmy_web_div_click();
+            console.log(html);
+            
+            cargarCarousel(carousel);
+
+        },
+        error: function(res) {
+            console.log(res);
+        },
+        data: {}
+    });
+}
+
+
+
+
+$(document).ready(function(){
+    serviciosws2();
+});
 });
