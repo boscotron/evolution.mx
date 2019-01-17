2c8372fad4fe8c8769d69951dccc07e1edb186be
boscotron <boscotron@users.noreply.github.com>
2019-01-17 14:43:11 -0600

    +ch bsk

--- app/controlador/jmyWebAjG.php
+++ app/controlador/jmyWebAjG.php
@@ -2,8 +2,9 @@
 if($jmyWeb->sesion()){
 	$o=["error"=>"datos insuficientes"];
 	$p=$jmyWeb->modulos(["modulos_permisos"=>true]);
-	$ta=($_POST['tabla']!='')?explode('__',$_POST['tabla']):['vistaweb'];
-	$t=(is_array($ta))?$ta[0]:'vistaweb';
+	$ta=($_POST['tabla']!='')?$_POST['tabla']:['vistaweb'];
+	$t=explode('__',$ta);
+	$t=(is_array($t))?$ta[0]:'vistaweb';
 	if($_POST['pagina']!=''&&$_POST['id']!=''&&$_POST['valor']!='' && ($_SESSION['JMY3WEB'][DOY] || $p['modulos_permisos'][$t]>1)){
 		$o=$jmyWeb->guardar(['pagina'=>$_POST['pagina'],'id'=>$_POST['id'],'valor'=>$_POST['valor'],'tabla'=>$ta,'opciones'=>$_POST['opciones']]);
 		if($_POST['opciones']['href']!='')
